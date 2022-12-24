1796a24ac81ca6252b28b832c51aa91377df66e5
yatharth <yatharthverma070@gmail.com>
2022-12-24 21:21:13 +0530

    added guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guessing game!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut input = String::new();    
+
+        // --snip--
+        io::stdin().read_line(&mut input).expect("Failed to read input");
+
+        let input: i32 = match input.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match input.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!"); 
+                break;
+            }
+        }
+    }
+    
 }

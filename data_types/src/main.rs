e56df0d791fa04a976151a27a61b01ea95c5e414
yatharth <yatharthverma070@gmail.com>
2023-01-28 21:13:30 +0530

    feat: Add some exampels

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 2.0; // f64 by default
+
+    let y : f32 = 3.0; // f32
+
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("Sum: {sum}");
+    println!("Product: {product}");
+    println!("Subtraction: {difference}");
+    println!("Quotient: {quotient}");
+    println!("Truncated: {truncated}");
+    println!("Remainder: {remainder}");
+
 }
